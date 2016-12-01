776fed7070d4795d1df9084ba4d2c80d6cffb2ee
Lucas Acosta <lucasmacosta@gmail.com>
2016-12-01 17:25:40 -0300

    Initial commit

---
+++ app/Http/Requests/StoreUserPost.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreUserPost extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        // We'll handle authorization in other place
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        // Email is unique among table users
+        $email_rule = 'required|email|max:255|unique:users';
+        if ($this->method() === 'PUT') {
+            // When updating, omit email from current user during unique check
+            $email_rule .= ',email,' . $this->route('user')->id;
+        }
+        return [
+            'name' => 'required|max:255',
+            'email' => $email_rule,
+            'password' => 'required|min:6|confirmed',
+            'role' => 'in:user,usersManager,admin',
+            'calories_per_day' => 'integer|min:0',
+        ];
+    }
+}
---
+++ database/migrations/2016_12_01_182119_create_settings_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSettingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('settings', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('calories_per_day');
+            $table->timestamps();
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')
+                ->references('id')->on('users')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('settings');
+    }
+}
